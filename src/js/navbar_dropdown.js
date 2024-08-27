3ee8be45d3be1844a9816fc56a6014eef1a49c5c
ActuallyAdequate <actuallyadequate@gmail.com>
2024-08-27 18:57:31 +1000

    Navbar

--- src/js/navbar_dropdown.js
+++ src/js/navbar_dropdown.js
@@ -2,9 +2,13 @@ function toggleNav() {
     const navbarList = document.getElementById('navbar-list');
     navbarList.classList.toggle("active");
     navbarList.style.display = navbarList.classList.contains('active') ? 'flex' : 'none';
- 
-    const icon = document.querySelector('.navbar-toggle');
-    icon.classList.toggle('active');
+    
+
+    const navbarToggle = document.getElementById('navbar-toggle');
+    navbarToggle.classList.toggle('active');
+    navbarToggle.setAttribute("hx-get", (navbarToggle.classList.contains('active') ? '/src/assets/close.svg' : '/src/assets/hamburger_menu.svg'));
+    htmx.process("#navbar-toggle");
+    htmx.trigger("#navbar-toggle", 'load');
 }
 
 window.addEventListener("resize", (event) => {
